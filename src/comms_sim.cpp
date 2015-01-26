a8790564ea143fc46f512b4c6c4a36431e13d2af
Nick Tsiogkas <nt95@hw.ac.uk>
2015-01-26 18:15:03 +0000

    Added basic method infrastructure.

--- src/comms_sim.cpp
+++ src/comms_sim.cpp
@@ -8,3 +8,22 @@
 
 #include <comms_sim/comms_sim.h>
 
+void CommsSim::createPubSub(std::string node_name)
+{
+
+}
+
+CommsSim::CommsSim(ros::NodeHandlePtr nhp)
+{
+
+}
+
+void CommsSim::init()
+{
+
+}
+
+void CommsSim::doWork()
+{
+
+}
